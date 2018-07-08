9260da8885d692926c933f041efb3c486faa611c
sonusharma151 <40905992+sonusharma151@users.noreply.github.com>
2018-07-08 16:48:47 -0700

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,6 +12,9 @@ public class Greeter {
 
   }
 
+  /**
+  *This is abstract function.
+  */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
